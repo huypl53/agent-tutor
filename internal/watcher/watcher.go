39410141f736f24329543b0bf865cfde38bbd433
lee <codingman@email.com>
2026-04-01 13:10:59 +0700

    chore: replace Go with npm package scaffold

--- internal/watcher/watcher.go
+++
@@ -1,9 +0,0 @@
-package watcher
-
-import "context"
-
-// Watcher is the common interface for all watchers (file, terminal, git, etc.).
-type Watcher interface {
-	Start(ctx context.Context) error
-	Stop() error
-}
